aa0bb947723e5630100c43a383128ba5b77dc155
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-19 15:30:19 +0530

    feat: add swagger-ui for testing

--- config.js
+++ config.js
@@ -13,5 +13,13 @@ const config = {
   };
   
   const connection = mysql.createConnection(config.db);
+
+  connection.connect((error)=>{
+    if (!error) {
+        console.log("DB Conection SucessFull");
+    }else{
+        console.log("DB Connection Failed: "+JSON.stringify(error, undefined,2));
+    }
+})
   
   module.exports = {connection};
--- server.js
+++ server.js
@@ -1,32 +1,13 @@
 const express = require('express');
-const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
-const mysql = require('./config');
 const greetingRoutes = require('./routes/greeting_routes');
-const logger = require('./logger');
+const logger = require('./utility/logger');
+const swaggerOption = require ('./swagger.json')
 require('dotenv').config();
-
-const swaggerOption = {
-  defination: {
-    openapi: '3.0.0',
-    info: {
-      title: 'GreetingApp',
-      description: 'Greetinng app UI',
-      contect:{
-        name: "Deep"
-      },
-      version: '1.0.0',
-      servers:["http://localhost:3000"]
-    } 
-  },
-  apis: ['server.js']
-}
-
-const swaggerDocs = swaggerJSDoc(swaggerOption);
-app.use('/swagger-api', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const app = express();
 const port = process.env.PORT;
+
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -34,13 +15,7 @@ app.use(express.urlencoded({
   })
 );
 
-mysql.connection.connect((error)=>{
-    if (!error) {
-        console.log("DB Conection SucessFull");
-    }else{
-        console.log("DB Connection Failed: "+JSON.stringify(error, undefined,2));
-    }
-})
+app.use('/swagger-api', swaggerUI.serve, swaggerUI.setup(swaggerOption));
 
 app.use(greetingRoutes);
 
