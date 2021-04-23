b52629d847f49a9e1120077aa00026cb426eba23
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-24 01:59:44 +0530

    refactor: use ternary oprater insted of if else in service

--- services/greetServices.js
+++ services/greetServices.js
@@ -4,48 +4,31 @@ class GreetService {
 
   getData = (callback) => {
     modelObj.get((err, result) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, result);
-      }
+      err ? callback(err) : callback(null, result)
     });
   };
 
   getWithId = (data, callback) => {
     modelObj.getWithId(data, (err, result) => {
-      if (err) callback(err);
-      else callback(null, result);
+      err ? callback(err) : callback(null, result)
     });
   };
 
   deleteWithId = (data, callback) => {
     modelObj.deleteWithId(data, (err, result) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, result);
-      }
+      err ? callback(err) : callback(null, result)
     });
   };
 
   createNewData = (data, callback) => {
     modelObj.create(data, (err, result) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, result);
-      }
+      err ? callback(err) : callback(null, result)
     });
   };
 
   updateDataWithId = (data, callback) => {
     modelObj.update(data, (err, result) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, result);
-      }
+      err ? callback(err) : callback(null, result)
     });
   };
 }
